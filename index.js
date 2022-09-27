c73ac21a6870b5182049e9c4ced620202e8dd733
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-27 11:34:31 +0200

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const axios = require('axios');
 const express = require('express')
 const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.json())
+const URL = "https://swapi.dev/api/people/"
+async function getCharacters() {
+    const response = await axios.get(`${URL}`)
+    return response
+}
+getCharacters()
+app.get(`${getCharacters()}`)
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
