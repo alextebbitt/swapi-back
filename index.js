f164f3699b44a4b5b87d4d77c9dbf6fa48007b14
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-27 14:54:34 +0200

    routes

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
-
 app.use(express.json())
-const URL = "https://swapi.dev/api/people/"
+
 async function getCharacters() {
-    const response = await axios.get(`${URL}`)
-    return response
+    const response = await axios.get("https://swapi.dev/api/people/");
+    return response;
 }
-getCharacters()
-app.get(`${getCharacters()}`)
+//  async function getCharacters() {
+//      const URL = "https://swapi.dev/api/people/";
+//     return   axios.get(URL)
+// }
+// getCharacters().then((response) => {
+//     console.log(response.data)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+app.get('/', (req, res) => {
+    return res.send(getCharacters())
+});
+
+
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
