ebc794541764bf60238addf076ee1a48c597a2d9
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-27 23:20:34 +0200

    endpoints done

--- index.js
+++ index.js
@@ -11,16 +11,47 @@ async function getCharacters() {
     return response;
 }
 
-getCharacters().then((response) => {
-    console.log(response.data)
-}).catch((error) => {
-    console.log(error)
-})
+async function searchById(req) {
+    try {
+        const response = await axios.get(`https://swapi.dev/api/people/${req.params.id}`);
+        return response
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function searchByName(name) {
+    try {
+        const response = await axios.get(`https://swapi.dev/api/people/?search=${name}`);
+        return response
+    } catch (error) {
+        console.error(error)
+    }
+
+}
+
+// getCharacters().then((response) => {
+//     console.log(response.data)
+// }).catch((error) => {
+//     console.log(error)
+// })
 
 
 app.get('/', async (req, res) => {
-    let rest = await getCharacters();
-    return res.send(rest.data)})
+    let results = await getCharacters();
+    return res.send(results.data)
+});
 
+app.get('/:id', async (request, res) => {
+    let results = await searchById(request);
+    return res.send(results.data)
+});
+
+app.get('/search/:name', async (req, res) => {
+    console.log("this is req name", req.params.name)
+    let results = await searchByName(req.params.name);
+    console.log("results", results)
+    return res.send(results.data)
+});
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
