c29df9447877fec21e01b4a5dc21edda5e40187e
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-30 03:39:43 +0200

    clean up code

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 var parsedJSON = require('./extra_character.json');
-console.log(parsedJSON)
+const cors = require("cors");
 app.use(express.json())
-
+app.use(cors());
 
 async function getCharacters(page) {
-    if(page === undefined) {
+    if (page === undefined) {
         page = 1;
     }
     const response = await axios.get(`https://swapi.dev/api/people/?page=${page}`);
@@ -35,12 +35,6 @@ async function searchByName(name) {
 
 }
 
-// getCharacters().then((response) => {
-//     console.log(response.data)
-// }).catch((error) => {
-//     console.log(error)
-// })
-
 
 app.get('/characters/:page', async (req, res) => {
     let results = await getCharacters(req.params.page);
@@ -53,7 +47,7 @@ app.get('/:id', async (request, res) => {
 });
 
 app.get('/search/:name', async (req, res) => {
-    
+
     let results = await searchByName(req.params.name);
     return res.send(results.data)
 });
