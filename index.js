a7ccad016fd9361febb88cecf6528741898987c9
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-30 13:12:02 +0200

    extra character and cache

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
-var parsedJSON = require('./extra_character.json');
+let parsedJSON = require('./extra_character.json');
 const cors = require("cors");
 app.use(express.json())
 app.use(cors());
@@ -42,12 +42,14 @@ async function searchById(req) {
 
 async function searchByName(name) {
     try {
+        if (name.toLowerCase().startsWith("gro")) {
+            return [parsedJSON];
+        }
         const response = await axios.get(`https://swapi.dev/api/people/?search=${name}`);
-        return response
+        return response?.data?.results;
     } catch (error) {
         console.error(error)
     }
-
 }
 
 
@@ -56,15 +58,10 @@ app.get('/characters/:page', async (req, res) => {
     return res.send(results)
 });
 
-app.get('search/:id', async (request, res) => {
-    let results = await searchById(request);
-    return res.send(results.data)
-});
-
 app.get('/search/:name', async (req, res) => {
-
     let results = await searchByName(req.params.name);
-    return res.send(results.data)
+    console.log(results)
+    return res.send(results);
 });
 
 app.listen(PORT, console.log(`Server running on port ${ PORT }`));
